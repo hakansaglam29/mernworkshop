4f6c9b2f193145954c4f4eecd8f1551450316210
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-12-15 20:55:59 +0100

    added product router

--- routers/ProductRouter.js
+++ routers/ProductRouter.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const ProductController = require('../controllers/ProductController');
+
+
+router.post('/addProduct', ProductController.addProduct);
+
+router.get('/getProduct/:id', ProductController.getProduct); // call Product 
+
+router.post('/updateProduct', ProductController.updateProduct);
+
+router.get('/deleteProduct/:id', ProductController.deleteProduct);
+
+router.get('/', ProductController.getProducts);
+
+module.exports = router;
